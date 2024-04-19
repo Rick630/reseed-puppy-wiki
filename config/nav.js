0b45d827abeae709b2798d334564208e99dd72b2
zhoubanxian <13814896412@163.com>
2024-04-19 09:46:39 +0800

    2

---
+++ config/nav.js
@@ -0,0 +1,18 @@
+module.exports = [
+  { text: "主页", link: "/" },
+
+  {
+    text: '安装教程',
+    link: '',
+    items: [
+      { text: '稳定版', link: '/guide/install/start/' },
+      { text: '测试版', link: '/guide/dev/install/start/'},
+    ],
+  },
+  { text: "赞助", link: "/zhanzhu/" },
+  {
+    text: "反馈",
+    link: "(https://gitee.com/zxfsadly/reseed-puppy/issues)",
+  },
+  { text: "Docker", link: "https://hub.docker.com/r/szzhoubanxian/reseed-puppy" },
+];
